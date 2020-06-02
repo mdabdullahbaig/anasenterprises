4fb93cea811c4f287d2a2f66b2523353d6000005
Md Abdullah Baig <44049345+mdabdullahbaig@users.noreply.github.com>
2020-06-02 14:04:04 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,12 +1,12 @@
 module.exports = {
     google: {
-        clientID: '468435296002-j1aah7fu9j2vttsq73nsc6q6accrd51n.apps.googleusercontent.com',
-        clientSecret: 'icOfnXryPx8nz70Z4WUVwcbI'
+        clientID: 'clientID',
+        clientSecret: 'clientSecret'
     },
 
     mongodb: {
         
-        dbURL: 'mongodb://biharsuper50:Abdul7438@ds125821.mlab.com:25821/mydatabase',
+        dbURL: 'mongodb://<databasename>:<password>@ds125821.mlab.com:25821/mydatabase',
         //dbURL: 'mongodb://localhost:27017/BiharSuper50',
         useNewUrlParser: true
 
